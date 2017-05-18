779889d7b4a844d7bd098bce04e7f5503e3a298f
Baysiide <luketrager@yahoo.com>
2017-05-17 22:25:56 -0400

    Update ragnarok.py

--- ragnarok/ragnarok.py
+++ ragnarok/ragnarok.py
@@ -14,7 +14,7 @@ class RagnarokClass:
 
     @commands.command(name="ragnarok", pass_context=True)
     @commands.cooldown(5, 60, commands.BucketType.user)
-    async def _united(self, ctx, text):
+    async def _ragnarok(self, ctx, text):
         """Compilation #2 of commands for United Investors"""
         search_type = ctx.message.content[len(ctx.prefix+ctx.command.name)+1:].lower().split(" ")
         option = {
