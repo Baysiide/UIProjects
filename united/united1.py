247d1aae9787b2d7c4d6c3964f8a34bbcf4d7426
Baysiide <luketrager@yahoo.com>
2017-05-17 22:08:38 -0400

    Create united1.py

---
+++ united/united1.py
@@ -0,0 +1,47 @@
+from discord.ext import commands
+from random import choice
+from bs4 import BeautifulSoup
+import requests
+import aiohttp
+import re
+import urllib
+
+
+class UnitedInvestors1:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="united1", pass_context=True)
+    @commands.cooldown(5, 60, commands.BucketType.user)
+    async def _united(self, ctx, text):
+        """Compilation #2 of commands for United Investors"""
+        search_type = ctx.message.content[len(ctx.prefix+ctx.command.name)+1:].lower().split(" ")
+        option = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+        }
+        regex = [
+            re.compile(",\"ou\":\"([^`]*?)\""),
+            re.compile("<h3 class=\"r\"><a href=\"\/url\?url=([^`]*?)&amp;"),
+            re.compile("<h3 class=\"r\"><a href=\"([^`]*?)\""),
+            re.compile("\/url?url=")
+            ]
+        search_valid = str(ctx.message.content
+                           [len(ctx.prefix+ctx.command.name)+1:].lower())
+                           
+        if search_type[0] == "finviz":
+        url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
+        response = requests.get(url)
+        html = response.content
+
+        soup = BeautifulSoup(html)
+        table = soup.find('tbody', attrs={'class': 'stripe'})
+        await self.bot.say(table.prettify())
+        
+        else:
+        await self.bot.say('Say Finviz dummy)
+        
+
+def setup(bot):
+    n = UnitedInvestors1(bot)
+    bot.add_cog(n)
