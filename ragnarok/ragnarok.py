304668e267e2438ea3dea6da8fdfac0c24b370b2
Baysiide <luketrager@yahoo.com>
2017-05-17 23:40:58 -0400

    Update ragnarok.py

--- ragnarok/ragnarok.py
+++ ragnarok/ragnarok.py
@@ -29,21 +29,32 @@ class RagnarokClass:
         search_valid = str(ctx.message.content
                            [len(ctx.prefix+ctx.command.name)+1:].lower())
                            
-        if search_type[0] == "finviz":
-            url = "http://www.finviz.com/quote.ashx?t=cetx"
-            test = "Shs Float"
-            response = requests.get(url)
-            html = response.text
-            indexstring = html.find(test)
-            floatIndexStart = html.find("<b>", indexstring)
-            floatIndexEnd = html.find("</b>", indexstring)
-            floatString = html[floatIndexStart+3:floatIndexEnd] 
+        if search_type[0] == "float":
+            if search_valid == "float":
+                await self.bot.say("Please actually search something")
+            else:
+                quary = str(ctx.message.content
+                             [len(ctx.prefix+ctx.command.name)+6:].lower())
+                    encode = urllib.parse.quote_plus(quary, encoding='utf-8',
+                                                     errors='replace')
+                
+                url = "http://www.finviz.com/quote.ashx?t=" + encode
+                test = "Shs Float"
+                response = requests.get(url)
+                if response.status_code == 404:
+                     await self.bot.say("Stock not found. Please try again")
+                else:
+                    html = response.text
+                    indexstring = html.find(test)
+                    floatIndexStart = html.find("<b>", indexstring)
+                    floatIndexEnd = html.find("</b>", indexstring)
+                    floatString = html[floatIndexStart+3:floatIndexEnd] 
             
             
-            await self.bot.say(indexstring)
-            await self.bot.say(floatIndexStart)
-            await self.bot.say(floatIndexEnd)
-            await self.bot.say(floatString + " shares")
+                    await self.bot.say(indexstring)
+                    await self.bot.say(floatIndexStart)
+                    await self.bot.say(floatIndexEnd)
+                    await self.bot.say(floatString + " shares")
         
         else:
             await self.bot.say('Say Finviz dummy')
