72cb97c05ea7a69fec0cae6bcc55663c77c99ad8
Baysiide <luketrager@yahoo.com>
2017-05-20 01:03:27 -0400

    Update ragToS.py

--- ragToS/ragToS.py
+++ ragToS/ragToS.py
@@ -49,7 +49,7 @@ class RagnarokToS:
 
                 #Your code will go here
 
-                await self.bot.say(block1)
+                await self.bot.say(block1.get(quary))
         
         elif search_type[0] == "help":
             await self.bot.say("You get NOTHING. SHOO")
