450eae0e1c0bb8856d79e194ab851a0582f9f2ab
Baysiide <luketrager@yahoo.com>
2017-05-24 11:18:02 -0400

    Update ragadmin.py

--- ragadmin/ragadmin.py
+++ ragadmin/ragadmin.py
@@ -80,11 +80,14 @@ class Admin:
         server = ctx.message.server
         
         members = [x.name for x in server.members if x.name != "@everyone"]
+        for x in server.members
+            if x.name != "@everyone"
+                roles = [x.name for x in user.roles if x.name != "@everyone"]
+                await self.bot.say(x + roles)   
         #roles = [x for x in members if x.name != "@everyone"]
         #TrialEnd = self._role_from_string(server, ("Trial Ended")
         #trial_ended = [x.name for x in server.members if TrialEnd in x.roles]
-        await self.bot.say(members) 
-        await self.bot.say(x for x in members)   
+        await self.bot.say(members)    
         #await self.bot.say(trial_ended) 
         
         
