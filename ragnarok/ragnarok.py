83cb28979db5ce2ce52a43afb55ca8f6cd850604
Baysiide <luketrager@yahoo.com>
2017-06-01 11:22:41 -0400

    Update ragnarok.py

--- ragnarok/ragnarok.py
+++ ragnarok/ragnarok.py
@@ -467,10 +467,110 @@ class RagnarokClass:
                     percentChange5 = html[indexStart+1:indexEnd]
                     await self.bot.say("```Small Cap Swing Scanner by PummHead -- \n1. " + SymString1 + "\t" + percentChange1 +"\n2. " + SymString2 + "\t" + percentChange2 +"\n3. " + SymString3 + "\t" + percentChange3 +"\n4. " + SymString4 + "\t" + percentChange4 +"\n5. " + SymString5 + "\t" + percentChange5 +"```")
         
+        elif search_type[0] == "afiro":                 
+                url = "http://www.finviz.com/screener.ashx?v=111&f=geo_usa,ind_stocksonly,sh_avgvol_o100,sh_price_u5,ta_averagetruerange_o0.25,ta_volatility_wo3&ft=4&o=-change"
+                test = "screener-link"
+                response = requests.get(url)
+                if response.status_code == 404:
+                     await self.bot.say("Scan not found. Please try again")
+                else:
+                    
+                    html = response.text
+                    indexstring = html.find(test)
+                    html = html[indexstring+1:]
+                    
+                    indexstring = html.find(test)
+                    IndexStart = html.find(">", indexstring)
+                    IndexEnd = html.find("<", indexstring)
+                    SymString1 = html[IndexStart+1:IndexEnd]
+                    SymString1 = symStringLenCheck(SymString = SymString1)
+                    
+                    html = symToPerc(oriString = html, text = test)
+                                        
+                    indexstring = html.find("><")
+                    html = html[indexstring+2:]
+                    indexStart = html.find(">")
+                    indexEnd = html.find("<")
+                    percentChange1 = html[indexStart+1:indexEnd]
+                    
+                    #move to 2nd
+                    indexstring = html.find(">2<")
+                    html = html[indexstring:]
+                    
+                    indexstring = html.find(test)
+                    IndexStart = html.find(">", indexstring)
+                    IndexEnd = html.find("<", indexstring)
+                    SymString2 = html[IndexStart+1:IndexEnd]
+                    SymString2 = symStringLenCheck(SymString = SymString2)
+                    
+                    html = symToPerc(oriString = html, text = test)
+                    
+                    indexstring = html.find("><")
+                    html = html[indexstring+2:]
+                    indexStart = html.find(">")
+                    indexEnd = html.find("<")
+                    percentChange2 = html[indexStart+1:indexEnd]
+                    
+                    #3rd
+                    indexstring = html.find(">3<")
+                    html = html[indexstring:]
+                    
+                    indexstring = html.find(test)
+                    IndexStart = html.find(">", indexstring)
+                    IndexEnd = html.find("<", indexstring)
+                    SymString3 = html[IndexStart+1:IndexEnd]
+                    SymString3 = symStringLenCheck(SymString = SymString3)
+                    
+                    html = symToPerc(oriString = html, text = test)
+                    
+                    indexstring = html.find("><")
+                    html = html[indexstring+2:]
+                    indexStart = html.find(">")
+                    indexEnd = html.find("<")
+                    percentChange3 = html[indexStart+1:indexEnd]
+                    
+                    #4th
+                    indexstring = html.find(">4<")
+                    html = html[indexstring:]
+                    
+                    indexstring = html.find(test)
+                    IndexStart = html.find(">", indexstring)
+                    IndexEnd = html.find("<", indexstring)
+                    SymString4 = html[IndexStart+1:IndexEnd]
+                    SymString4 = symStringLenCheck(SymString = SymString4)
+                    
+                    html = symToPerc(oriString = html, text = test)
+                    
+                    indexstring = html.find("><")
+                    html = html[indexstring+2:]
+                    indexStart = html.find(">")
+                    indexEnd = html.find("<")
+                    percentChange4 = html[indexStart+1:indexEnd]
+                    
+                    #5th
+                    indexstring = html.find(">5<")
+                    html = html[indexstring:]
+                    
+                    indexstring = html.find(test)
+                    IndexStart = html.find(">", indexstring)
+                    IndexEnd = html.find("<", indexstring)
+                    SymString5 = html[IndexStart+1:IndexEnd]
+                    SymString5 = symStringLenCheck(SymString = SymString5)
+                    
+                    html = symToPerc(oriString = html, text = test)
+                    
+                    indexstring = html.find("><")
+                    html = html[indexstring+2:]
+                    indexStart = html.find(">")
+                    indexEnd = html.find("<")
+                    percentChange5 = html[indexStart+1:indexEnd]
+                    await self.bot.say("```Afiron Volatility Scanner -- \n1. " + SymString1 + "\t" + percentChange1 +"\n2. " + SymString2 + "\t" + percentChange2 +"\n3. " + SymString3 + "\t" + percentChange3 +"\n4. " + SymString4 + "\t" + percentChange4 +"\n5. " + SymString5 + "\t" + percentChange5 +"```")
+        
         #Start of help
         elif search_type[0] == "help":
             if search_valid == "help":
                 await self.bot.say("```Ragnarok's list of working commands-- \n~chart <ticker symbol> :: Shows image of daily chart \n~float <ticker symbol> :: Checks Finviz for float \n~insti <ticker symbol> :: Checks Finviz for institutional ownership \n~short <ticker symbol> :: Checks Finviz for short float \n~pgain :: List of Top % gainers on the day \n~plose :: List of Top % losers on the day" + "```")
+                await self.bot.say("```User made custom Finviz scans-- \n~pummh :: Swing scanner for small caps, over moving averages on daily with consistent volume \n~afiro :: Small cap volatility scanner " + "```")
         else:
             await self.bot.say('Unrecognized command. For options, type ~ragnarok help')
         
