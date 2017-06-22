7e562469095d4c015b1b1c2338a436f288e6499c
Baysiide <luketrager@yahoo.com>
2017-06-22 19:43:03 -0400

    Update ragadmin.py

--- ragadmin/ragadmin.py
+++ ragadmin/ragadmin.py
@@ -117,7 +117,7 @@ class Admin:
         server = ctx.message.server
         
         counter = 0
-        client = discord.Client()
+        #client = discord.Client()
         async for message in client.logs_from(channel, limit=100):
         #async for message in client.logs_from(channel, limit=100, before=None, after=None, around=None, reverse=False):
             #if message[:2] == "+$":
