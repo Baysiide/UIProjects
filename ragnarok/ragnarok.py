1e58862c239773ff2475591ca2b8659e742f4b95
Baysiide <luketrager@yahoo.com>
2017-06-22 22:43:35 -0400

    Update ragnarok.py

--- ragnarok/ragnarok.py
+++ ragnarok/ragnarok.py
@@ -7,14 +7,15 @@ import re
 import urllib
 
 counter = 0
+msgcount = 0
 class RagnarokClass:
 
     def __init__(self, bot):
         self.bot = bot
         
     async def proc_mess(self, message):
-        global counter
-        counter += 1
+        global msgcount
+        msgcount += 1
         
     @commands.command(name="ragnarok", pass_context=True)
     @commands.cooldown(10, 60, commands.BucketType.user)
