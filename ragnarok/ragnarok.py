28a2676a400604a2f11a70418d1addde2c1313fa
Baysiide <luketrager@yahoo.com>
2017-06-22 22:36:23 -0400

    Update ragnarok.py

--- ragnarok/ragnarok.py
+++ ragnarok/ragnarok.py
@@ -692,11 +692,7 @@ class RagnarokClass:
         else:
             await self.bot.say('Unrecognized command. For options, type ~ragnarok help')
             
-    async def proc_on_message(self, ctx.message):
-        global counter
-        counter +=1
 
 def setup(bot):
     n = RagnarokClass(bot)
-    bot.add_listener(n.proc_on_message, "on_message")
     bot.add_cog(n)
