9bf8058894acddf3ee3ad65ce8c35128f933ef3c
Baysiide <luketrager@yahoo.com>
2017-06-25 00:57:32 -0400

    Update ragnarok.py

--- ragnarok/ragnarok.py
+++ ragnarok/ragnarok.py
@@ -188,7 +188,8 @@ class RagnarokClass:
                     IndexEnd = html.find("</div>")
                     dataString = html[IndexStart+7:IndexEnd]
                     html = html[IndexEnd:]
-            
+                    
+                    await self.bot.say("*Dates shown are LATEST expected release dates. FDA data for these may be already released.*")
                     if y != x-1:
                         await self.bot.say(dateString + " - **" + compString + "**  -  " + priceString + "          Drug/Condition: " + drugString + "/" + conString + "\n" + urlString + "\n" + dataString + "\n------------------------------------------------------------------------")
                     else:
