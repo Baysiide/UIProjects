2add55a6bb276d387d851b71b6c393496fa0e75f
RagnarokIU <unitedStockAlerts@yahoo.com>
2017-08-28 16:30:14 -0400

    Update RagScraper.py

--- RagScraper/RagScraper.py
+++ RagScraper/RagScraper.py
@@ -34,69 +34,8 @@ class ScrClass:
                            [len(ctx.prefix+ctx.command.name)+1:].lower())
         
         
-        #Start of float calculations                   
-        if search_type[0] == "float":
-            if search_valid == "float":
-                await self.bot.say("Please add the the ticker symbol for data")
-            else:
-                quary = str(ctx.message.content
-                            [len(ctx.prefix+ctx.command.name)+6:].lower())
-                encode = urllib.parse.quote_plus(quary, encoding='utf-8',
-                                                 errors='replace')
-                
-                url = "http://www.finviz.com/quote.ashx?t=" + encode
-                test = "Shs Float"
-                response = requests.get(url)
-                if response.status_code == 404:
-                     await self.bot.say("Stock not found. Please try again")
-                else:
-                    html = response.text
-                    indexstring = html.find(test)
-                    floatIndexStart = html.find("<b>", indexstring)
-                    floatIndexEnd = html.find("</b>", indexstring)
-                    floatString = html[floatIndexStart+3:floatIndexEnd] 
-                    if floatString == "-":
-                        await self.bot.say(encode[1:].upper() + " does not have listed float data on Finviz.")
-                    elif floatString[0] == "<":
-                        exIndex = floatString.find(">")
-                        endIndex = floatString.find("</s")
-                        floatString = floatString[exIndex+1:endIndex]
-                        await self.bot.say(encode[1:].upper() + " has a float of " + floatString + ".")
-                    else:
-                        await self.bot.say(encode[1:].upper() + " has a float of " + floatString + " shares.")
-                        
-        elif search_type[0] == "inside":
-            if search_valid == "inside":
-                await self.bot.say("Please add the the ticker symbol for data")
-            else:
-                quary = str(ctx.message.content
-                            [len(ctx.prefix+ctx.command.name)+7:].lower())
-                encode = urllib.parse.quote_plus(quary, encoding='utf-8',
-                                                 errors='replace')
-                
-                url = "http://www.finviz.com/quote.ashx?t=" + encode
-                test = "<li class=\"oagrid-item  highiv earnings\">"
-                response = requests.get(url)
-                if response.status_code == 404:
-                     await self.bot.say("Stock not found. Please try again")
-                else:
-                    html = response.text
-                    indexstring = html.find(test)
-                    floatIndexStart = html.find("<b>", indexstring)
-                    floatIndexEnd = html.find("</b>", indexstring)
-                    floatString = html[floatIndexStart+3:floatIndexEnd] 
-                    if floatString == "-":
-                        await self.bot.say(encode[1:].upper() + " does not have listed insider data on Finviz.")
-                    elif floatString[0] == "<":
-                        exIndex = floatString.find(">")
-                        endIndex = floatString.find("</s")
-                        floatString = floatString[exIndex+1:endIndex]
-                        await self.bot.say(encode[1:].upper() + " has an insider ownership of " + floatString + ".")
-                    else:
-                        await self.bot.say(encode[1:].upper() + " has an insider ownership of " + floatString + ".")
         
-        
-        elif search_type[0] == "alpha":
+        if search_type[0] == "alpha":
             if search_valid == "alpha":
                 await self.bot.say("Please add post preference variable")
             else:
@@ -113,10 +52,10 @@ class ScrClass:
                 values = {'log': 'collinamedee@gmail.com',
                           'pwd': encode}
                 r = s.post(url1, data=values)
-                await self.bot.say(r.url)
+                #await self.bot.say(r.url)
                 url = "https://optionalpha.com/members/watch-list"
                 response = s.get(url)
-                if r.url == "https://optionalpha.com/members":
+                if r.url != "https://optionalpha.com/members":
                      await self.bot.say("Variable not found. Please try again")
                 else:
                     x = 6
