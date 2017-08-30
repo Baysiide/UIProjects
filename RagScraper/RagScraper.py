13846483be078cde2264f5f12e6a42a758423398
RagnarokIU <unitedStockAlerts@yahoo.com>
2017-08-30 12:45:32 -0400

    Update RagScraper.py

--- RagScraper/RagScraper.py
+++ RagScraper/RagScraper.py
@@ -176,7 +176,7 @@ class ScrClass:
                 finalString = ""
                 i = 0
                 while i < len(list1):
-                    await self.bot.say(i)
+                    await self.bot.say(i+1 % 5)
                     if i+1 % 5 == 0:
                         await self.bot.say(list1[i] + "Gotcha")
                         finalString = finalString + list1[i] + "\n"
