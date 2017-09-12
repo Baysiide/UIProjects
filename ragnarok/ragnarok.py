5c898174cc38207dd3a27e35f15519fc27ae835f
RagnarokIU <unitedStockAlerts@yahoo.com>
2017-09-12 12:43:29 -0400

    Update ragnarok.py

--- ragnarok/ragnarok.py
+++ ragnarok/ragnarok.py
@@ -17,7 +17,8 @@ class RagnarokClass:
         global messcount
         messcount = messcount + 1        
         if ctx.message.content.startswith('@everyone'):
-            await self.bot.send_message(discord.Object(id='311323578626867211'), text)
+            #await self.bot.send_message(discord.Object(id='311323578626867211'), text)
+            await self.bot.say("hello")
 
         
         
